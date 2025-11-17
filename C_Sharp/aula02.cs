250c728a3130b41df3ebcc39bb25c9ee83ded3ba
Daniel <55755085+danzinho007@users.noreply.github.com>
2025-11-17 15:54:08 -0300

    add

---
+++ C_Sharp/aula02.cs
@@ -0,0 +1,14 @@
+using System;
+namespace HelloWord
+{
+class Principal{
+    static void Main(){
+        Console.WriteLine("Ola Mundo!");
+        Console.WriteLine("Me livrei da maldicao");
+        Console.Write("Somando 3 com 3 da : ");
+        Console.WriteLine(3 + 3);
+        Console.Write("Isso");
+        Console.Write(" escreve na mesma linha");
+        }
+    }
+}
