b647d1d3a9ce589db0cc41c2c9b058d6071ae60a
Daniel <55755085+danzinho007@users.noreply.github.com>
2025-11-19 16:58:07 -0300

    add

---
+++ C-PlusPlus/Aula01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Hello World";
+
+    return 0;
+}
+
+ou 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    return 0;
+}
